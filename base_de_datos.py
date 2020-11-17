85cdb158492e8255191d085e062c9d1916d6c085
ramon hernandez <ramonhernandez102703@gmail.com>
2020-11-16 19:16:31 -0600

    Changes DB

--- base_de_datos.py
+++ base_de_datos.py
@@ -5,17 +5,26 @@ cursor = conection.cursor()
 
 #cursor.execute("CREATE TABLE USER(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME VARCHAR(20) UNIQUE, OLDYEAR INTEGER)")
 def agregar():
-    name = str(input('Your name: '))
-    old_year = int(input('Your old year: '))
-    variable = [
-        (name,old_year)
-        ]
-    cursor.executemany("INSERT INTO USER VALUES (NULL,?,?)", variable)
+    try:
+        cantidad = int(input('Cuantos datos desea agregar: '))
+        i = 1
+        while i <= cantidad:
+            i = i + 1
+            name = str(input('Your name: '))
+            old_year = int(input('Your old year: '))
+            print('='*10, '\n', 'Nuevos Datos', '\n','='*10)
+            variable = [
+                (name,old_year)
+                ]
+            cursor.executemany("INSERT INTO USER VALUES (NULL,?,?)", variable)
+            conection.commit()
+    except:
+        print('Error De valores')
 def mostrar():
     cursor.execute("SELECT * FROM USER")
     variable_users = cursor.fetchall()
     for user in variable_users:
-        print('ID user: ', user[0], 'Name user: ', user[1], 'Old year user: ', user[2])
+        print('ID user: ', user[0], '||','Name user: ', user[1], '||','Old year user: ', user[2])
 
 print('1.Add to data base', '2. show data base')
 
@@ -30,6 +39,8 @@ try:
 except:
     print('Opcion de valor invalida')
 
+cursor.execute("UPDATE USER SET NAME=Fred WHERE ID='7'")
+print('Cambiado')
 conection.commit()
 conection.close()
 print('Finish')
