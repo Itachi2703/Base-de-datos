e5932cee7193e693063b56d914e9ff5a152b718d
ramon <ramonhernandez102703@gmail.com>
2020-12-06 12:58:37 -0600

    iu modified

--- iu.py
+++ iu.py
@@ -1,10 +1,11 @@
 import tkinter
-
+import random
 win = tkinter.Tk()
 win.geometry("400x300")
 etiqueta = tkinter.Label(win, text = "Hola")
 def runfoR():
-	print("Hola")
+	print("Hola buen dia")
+	
 boton1 = tkinter.Button(win, text = "myfunc", command = runfoR)
 boton1.pack()
 win.mainloop()
